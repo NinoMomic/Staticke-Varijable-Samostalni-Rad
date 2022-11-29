1fe238e3284506f5a61442660736241ec2080757
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2022-11-29 23:41:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticke_varijable
+{
+    class PrvaKlasa
+    {
+        private static readonly int counter = 0;
+
+        public static int Counter { get => counter; }
+
+        static PrvaKlasa()
+        {
+            counter++;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+
+            Console.WriteLine(PrvaKlasa.Counter);
+
+            Console.ReadKey();
+        }
+    }
+}
